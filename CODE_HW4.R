b3d3bb55808bc522048e4cc4d57c30f809caeb39
Mirai <djmf.mmf@gmail.com>
2014-03-16 22:20:35 -0700

    Question 3.3
    Done question 3.3 by Mirai

--- CODE_HW4.R
+++ CODE_HW4.R
@@ -1,4 +1,47 @@
+################################################################################
 ### Homework Chapter 3-1
 ### Mirai Furukawa, Scott Thomas, Peter Varshavsky
 ### 2014/03/20
 ### Questions: 3.3,3.5,3.8,3.9
+
+
+
+### Question 3.3
+
+#a. State the prediction equation, and interpret the intercept and slope.
+
+########################################
+# Mirai Begin                          #   
+########################################
+
+cat("The prediction equation is Y = 0.00109x+ 0.00255")
+
+#b. Use the model fit to estimate the (i) probabilities of malformation for 
+#alcohol levels 0 and 7.0, (ii) relative risk comparing those levels.
+
+cat("probability of malformation for alcohol level of 0 is \nY = 0.00109(0) + 0.00255 = 0.00255")
+cat("Y = 0.00109(0) + 0.00255 = 0.00255")
+cat("probability of malformation for alcohol level of 7 is")
+cat("Y = 0.00109(7) + 0.00255 = ", 0.00109*7 + 0.00255)
+
+cat("Relative risk of x = 0 to x = 7 is",0.00109*7 + 0.00255,"/ 0.00255 = ",(0.00109*7 + 0.00255)/0.0255)
+
+########################################
+# Mirai End                            #    
+########################################
+
+
+
+
+
+
+
+### Question 3.5
+
+
+
+### Question 3.8
+
+
+
+### Question 3.9
