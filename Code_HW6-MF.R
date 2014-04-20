6db7d2c996ad055c37c3b8860b0600c9c028ebc2
Mirai <djmf.mmf@gmail.com>
2014-04-20 11:05:05 -0700

    Just started Coding
    Started solving question 4.24

---
+++ Code_HW6-MF.R
@@ -0,0 +1,14 @@
+################################################################################
+### Homework Chapter 4-1
+### Mirai Furukawa, Scott Thomas, Peter Varshavsky
+### 2014/04/22
+### Questions: 4.1,4.2,4.4,4.11,4.14,4.16,4.17,4.24
+
+
+
+
+### Question 4.24
+
+library(icda)
+data(throat)
+throat.24 = throat
