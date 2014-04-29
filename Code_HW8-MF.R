15d9b98f5528f2bdda15125a22ceb65fbd5e7529
Mirai <djmf.mmf@gmail.com>
2014-04-28 17:58:08 -0700

    Started Coding for HW8
    Coding for 5.4 is done. Started working on 5.19 skipping 5.15 for now.

---
+++ Code_HW8-MF.R
@@ -0,0 +1,16 @@
+################################################################################
+### Homework Chapter 5
+### Mirai Furukawa, Scott Thomas, Peter Varshavsky
+### 2014/04/22
+### Questions: 5.4,5.15,5.19,5.22,5.30
+
+### Question 5.4
+pchisq(11.1419, 11, lower.tail=FALSE)
+pchisq(7.4091, 6, lower.tail=FALSE)
+
+
+### Question 5.15
+
+
+
+### Question 5.19
