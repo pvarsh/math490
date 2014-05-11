14661e8eb5d0245eebc6d43e3b0a641b8e3f3757
Peter Varshavsky <pvarsh@gmail.com>
2014-05-11 12:25:37 -0700

    Working on generating data.

--- 490-Presentation-code-PV.R
+++ 490-Presentation-code-PV.R
@@ -3,26 +3,40 @@
 
 ### Generating meaningful predictors
 
-N = 100 # number of observations
+N = 1000 # number of observations
 p.signal = 3 # number of signal predictors
 p.noise = 40 # number of noise predictors
 p = p.signal + p.noise
 
 df = data.frame(matrix(nrow = N, ncol = p + 1)) # initialize the data frame to dimensions p + 1, N
 
-set.seed(4321) # setting the seed for reproducibility
+#set.seed(4321) # setting the seed for reproducibility
 for (i in seq(p.signal)){
   df[[i + 1]] = rnorm(N, 0, 1)
 }
 
-#beta = runif(n = p.signal, min = 1, max = 3)
-beta = c(1,2,3)
+colnames(df)[1] = "Y" # response variable will be in the first column of df
+
+# generating coefficients
+beta = c(10,-4,3) # beta should be the same length as p.signal
 beta.matrix = t(replicate(N, beta))
-beta.matrix
-y = beta.matrix * as.matrix(df[2:(1+p.signal)]) + rnorm(N, 0, .3)
-head(y)
 
+# with error terms
+logit.Y = rowSums(beta.matrix * as.matrix(df[2:(1+p.signal)]) + rnorm(N, 0, 1))
+# without error terms
+#logit.Y = rowSums(beta.matrix * as.matrix(df[2:(1+p.signal)]))
 
+Y = as.numeric(logit.Y > .5)
+df[[1]] = Y
+
+cor(df$Y, df$X4)
+par(mfrow = c(3,1))
+for (i in 2:4){
+  plot(df[[i]], df[[1]])  
+}
+
+mod1 = glm(Y ~ X2 + X3 + X4, family = binomial(link = "logit"), data = df)
+summary(mod1)
 ### Generating noise predictors
 
 
