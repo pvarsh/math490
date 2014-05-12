82c111ee7358b3b896bd663c4d77496006096a48
Peter Varshavsky <pvarsh@gmail.com>
2014-05-11 18:10:20 -0700

    Added lasso fits

--- 490-Presentation-code-PV.R
+++ 490-Presentation-code-PV.R
@@ -6,7 +6,7 @@ library(glmnet)
 
 N = 100 # number of observations
 p.signal = 3 # number of signal predictors
-p.noise = 40 # number of noise predictors
+p.noise = 10 # number of noise predictors
 p = p.signal + p.noise
 
 df = data.frame(matrix(nrow = N, ncol = p + 2)) # initialize the data frame to dimensions p + 1, N
@@ -28,7 +28,7 @@ head(df)
 colnames(df)[1] = "Y" # response variable will be in the first column of df
 
 ### Generating coefficients
-beta = c(1,-1,2) # beta should be the same length as p.signal
+beta = c(1,-1,0.5) # beta should be the same length as p.signal
 beta.matrix = t(replicate(N, beta))
 
 ### Building the dependent variable
@@ -58,3 +58,21 @@ mod1 = glm(Y ~ X2 + X3 + X4 + X5 + X6 + X7 + X8 + X9 + X10 + X11 + X12, family =
 summary(mod1)
 
 
+### Fitting lasso
+
+# Transforming data to matrix and vector form
+# according to the vignette http://www.stanford.edu/~hastie/glmnet/glmnet_alpha.html
+# glmnet wants the predictors to be in a matrix and the outcomes as a vector
+x = as.matrix(df[2:(p + 2)]) # predictors
+y = as.numeric(df[[1]])
+
+# fitting lasso regression
+mod.lasso = glmnet(x, y, family = "binomial")
+par(mfrow = c(1,1)) # one row one column plot layout
+plot(mod.lasso) # plots graph of coefficients as a function of L1 norm
+coef(mod.lasso, 0.09)
+cv.mod = cv.glmnet(x, y, family = "binomial")
+plot(cv.mod)
+#cv.mod$lambda.1se
+coef(cv.mod, s = "lambda.1se")
+coef(cv.mod, s = "lambda.min")
